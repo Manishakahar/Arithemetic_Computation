a75b613307d3977cd0ec8274f96979ebf015f16d
rajnish432 <rajnish.kahar1996@gmail.com>
2021-11-14 22:12:53 +0530

    [Manisha]: Store Operations In Dictionary.

--- arithmetic_comp.sh
+++ arithmetic_comp.sh
@@ -3,15 +3,17 @@ echo "Welcome to arithmetic computation"
 read -p "Enter three values for a,b,c: " a b c
 echo "$a $b $c"
 
+declare -A dict
+
 #!Calcuting Results Of Arithmetic Operation
-operation1=$((a + b * c))
-operation2=$((a * b + c))
-operation4=$((a % b + c))
+dict[operation1]=$((a + b * c))
+dict[operation2]=$((a * b + c))
+dict[operation4]=$((a % b + c))
 #! Used scale=2 for display .2 floating value
-operation3=$(echo "scale=2; $c + $a / $b" | bc )
+dict[operation3]=$(echo "scale=2; $c + $a / $b" | bc )
 
 #!Printing Operation and Result
-echo "$a + $b * $c = $operation1"
-echo "$a * $b + $c = $operation2"
-echo "$c + $a / $b = $operation3"
-echo "$a % $b + $c = $operation4"
+echo "$a + $b * $c = ${dict[operation1]}"
+echo "$a * $b + $c = ${dict[operation2]}"
+echo "$c + $a / $b = ${dict[operation3]}"
+echo "$a % $b + $c = ${dict[operation4]}"
