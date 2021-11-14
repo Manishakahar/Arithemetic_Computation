deb4071202548f4a39f9ce4318a376ee3bab7e86
rajnish432 <rajnish.kahar1996@gmail.com>
2021-11-14 22:03:06 +0530

    [Manisha]: Perform Arithmetic Computation

--- arithmetic_comp.sh
+++ arithmetic_comp.sh
@@ -2,3 +2,16 @@ echo "Welcome to arithmetic computation"
 
 read -p "Enter three values for a,b,c: " a b c
 echo "$a $b $c"
+
+#!Calcuting Results Of Arithmetic Operation
+operation1=$((a + b * c))
+operation2=$((a * b + c))
+operation4=$((a % b + c))
+#! Used scale=2 for display .2 floating value
+operation3=$(echo "scale=2; $c + $a / $b" | bc )
+
+#!Printing Operation and Result
+echo "$a + $b * $c = $operation1"
+echo "$a * $b + $c = $operation2"
+echo "$c + $a / $b = $operation3"
+echo "$a % $b + $c = $operation4"
